abce07ae275da150e599c6040d66e7bf1f02c472
Skyler <skyler.m.degrote@outlook.com>
2015-08-17 16:03:38 -0500

    added header, main, and footer sections

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -1,11 +1,11 @@
 personalWebsiteApp.controller("HomeController", ["$scope", function($scope){
-    console.log("home controller works!");
+
 }]);
 
 personalWebsiteApp.controller("AboutController", ["$scope", function($scope){
-    console.log("About controller works!");
+
 }]);
 
 personalWebsiteApp.controller("ProjectsController", ["$scope", function($scope){
-    console.log("Projects controller works!");
+
 }]);
